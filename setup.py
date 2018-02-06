bb6f44a4c75b15108ce3f659245d875e372ec31a
jordiprats <jordi.prats@gmail.com>
2018-02-06 19:35:21 +0100

    base penis ipsum

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'penisipsum',
+  packages = ['penisipsum'], # this must be the same as the name above
+  version = '2018.02',
+  description = 'penis ipsum',
+  author = 'Jordi Prats',
+  author_email = 'jprats@systemadmin.es',
+  url = 'https://github.com/jordiprats/python-penisipsum', # use the URL to the github repo
+  download_url = 'https://github.com/jordiprats/python-penisipsum/archive/2018.02.tar.gz', # I'll explain this in a second
+  keywords = ['penis', 'ipsum', 'textgenerator', 'sample'], # arbitrary keywords
+  classifiers = [],
+)
+
