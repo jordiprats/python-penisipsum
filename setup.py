93ca1b0947f08720c4073acd8a5e2f65139d0d5e
Jordi Prats <jordi.prats@gmail.com>
2020-01-28 12:51:29 +0100

    setup module

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'penisipsum',
   packages = ['penisipsum'], # this must be the same as the name above
-  version = '2018.02',
+  version = '2019.06',
   description = 'penis ipsum',
+  long_description=long_description,
   author = 'Jordi Prats',
   author_email = 'jprats@systemadmin.es',
   url = 'https://github.com/jordiprats/python-penisipsum', # use the URL to the github repo
