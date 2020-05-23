8f8443a1e0d10b1d2e3f36ce6a41bc3178e8f711
jordiprats <jordi.prats@gmail.com>
2020-05-23 18:37:54 +0200

    2020.7.2

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-setup(
+setuptools.setup(
   name = 'penisipsum',
   packages = ['penisipsum'], # this must be the same as the name above
-  version = '2019.06',
-  description = 'penis ipsum',
-  long_description=long_description,
+  version = '2020.7.2',
   author = 'Jordi Prats',
   author_email = 'jprats@systemadmin.es',
-  url = 'https://github.com/jordiprats/python-penisipsum', # use the URL to the github repo
-  download_url = 'https://github.com/jordiprats/python-penisipsum/archive/2018.02.tar.gz', # I'll explain this in a second
-  keywords = ['penis', 'ipsum', 'textgenerator', 'sample'], # arbitrary keywords
-  classifiers = [],
+  description = 'penis ipsum',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url = 'https://github.com/jordiprats/python-penisipsum',
+  classifiers = [
+    "Operating System :: OS Independent",
+  ],
 )
-
